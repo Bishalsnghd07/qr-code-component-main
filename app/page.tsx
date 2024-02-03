f1a38a621080bcae826fe0db5ffebaff8df08ae4
Bishal Singh Deo <bishalsinghdeo5@gmail.com>
2024-02-03 11:26:36 +0530

    feat: update div tag

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
          height={280}
          className="rounded-2xl"
          />
-         <div className="pt-4 pl-1 pr-1 text-center justify-center items-center text-[2.4vh] lg:text-[1.6vw] font-bold text-black">Improve your front-end skills by building projects</div>
+         <h1 className="pt-4 pl-1 pr-1 text-center justify-center items-center text-[2.4vh] lg:text-[1.6vw] font-bold text-black">Improve your front-end skills by building projects</h1>
          <p className="text-[0.8rem] pl-2 pr-2 text-center text-slate-400 pt-2 pb-4">Scan the QR code to visit Frontend Mentor and take your coding to the next level</p>
       </div>
     </main>
